ae5c0ba7401e76c23da3bb600d59e104c881b025
Lucas <lsdeabreu@gmail.com>
2024-01-28 22:45:32 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const fazerCriptografia = (texto) => {
+    // As "chaves" de criptografia que utilizaremos são:
+    // A letra "e" é convertida para "enter"
+    // A letra "i" é convertida para "imes"
+    // A letra "a" é convertida para "ai"
+    // A letra "o" é convertida para "ober"
+    // A letra "u" é convertida para "ufat"
+
+    // Requisitos:
+    // - Deve funcionar apenas com letras minúsculas
+    // - Não devem ser utilizados letras com acentos nem caracteres especiais
+    // - Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
+
+    // Por exemplo:
+    // "gato" => "gaitober"
+    // gaitober" => "gato"
+}
+
+const criptografar = () => {
+    texto = document.getElementById('texto').value
+    console.log(texto)
+}
+
+const descriptografar = () => {
+    
+}
